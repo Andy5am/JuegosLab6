ed495fce44d9346e0e8a77c14b430e4e87a62b48
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-05-06 18:41:59 -0600

    creacion del nivel

---
+++ Lab6/Assets/enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+
+    public GameObject[] positions;
+    private int position = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshAgent>().autoBraking = false;
+
+        patrol();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GetComponent<NavMeshAgent>().pathPending && GetComponent<NavMeshAgent>().remainingDistance < 0.5f)
+        {
+            patrol();
+        }
+    }
+
+    void patrol()
+    {
+        GetComponent<NavMeshAgent>().destination = positions[position].transform.position;
+
+        if (position < 4)
+        {
+            position++;
+        }
+        else
+        {
+            position = 0;
+        }
+    }
+}
